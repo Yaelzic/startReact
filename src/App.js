85c354350608bf4d5adeea79a287349986ca8580
Yaelzic <heler7000@gmail.com>
2022-06-22 20:49:15 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import './App.css';
+import { useState } from 'react'
+import Names from './Names';
+
+const App = () => {
+
+  const [count, setCount] = useState(0)
+  const myStyle = { colore: "white", backgroundColor: "red" }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      
+        <button style={myStyle} onClick={() => setCount(count + 10)}>{count}</button>
+      
+      <Names userName="Yael"></Names>
+  
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/Names.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Names = ({userName}) => {
+  return (
+    <div>{userName}</div>
+  )
+}
+
+export default Names
